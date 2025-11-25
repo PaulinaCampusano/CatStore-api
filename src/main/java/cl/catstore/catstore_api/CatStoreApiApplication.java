ce166335dbef66431935359f5810fcc227bc9c86
PaulinaCampusano <pau.campusano@duocuc.cl>
2025-11-25 19:06:30 -0300

    Initial Spring Boot backend

---
+++ src/main/java/cl/catstore/catstore_api/CatStoreApiApplication.java
@@ -0,0 +1,13 @@
+package cl.catstore.catstore_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatStoreApiApplication.class, args);
+	}
+
+}
